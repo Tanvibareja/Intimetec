ba0f35e0c729a2910d627c46a0532e8b097a9307
Tanvibareja <126067110+Tanvibareja@users.noreply.github.com>
2024-12-16 15:51:15 +0530

    Updated code for  Eval_expr.c (Calculator program)
    Considered the comments on pr.
    Added comments to explain function logic.

--- Eval_expr.c
+++ Eval_expr.c
@@ -5,7 +5,7 @@
 #include <ctype.h>
 
 
-int Op_eval(int a, int b, char op) {
+int evaluateSingleOperation(int a, int b, char op) {  // This function evaluates a single operation like a+b
     switch (op)
     {
     case '+': return a+b;
@@ -25,40 +25,51 @@ int Op_eval(int a, int b, char op) {
     }
 }
 
-int op_Check(char ch) {
+int isOperator(char ch) {                                         // Checks if a character from the string is an operator
     return ch == '+' || ch == '-' || ch == '*' || ch == '/';
 }
 
-int Exp_sol(char* expr) {
-    int val[MAX], tp_val = -1;
-    char ops[MAX], tp_op = -1;
+int solveCompleteExpression(char* expr) {
+    int value_stack[MAX], value_stack_top = -1;
+    char operator_stack[MAX], operator_stack_top = -1;
 
     for (int i = 0; expr[i] != '\0'; i++) {
         if (isspace(expr[i])) continue;
 
-        if (op_Check(expr[i])) {
-            while (tp_op >= 0 && (
-                (expr[i] == '+' || expr[i] == '-') || 
-                ((expr[i] == '*' || expr[i] == '/') && (ops[tp_op] == '*' || ops[tp_op] == '/'))
-            )) {
-                if (tp_val < 1) {
+
+// if operator stack empty then push the operator into the operator stack
+
+// if operator stack not empty and current operator is '+'or '-' then pop value stack -> pop operator -> perform operation -> push value and operator
+
+/* if operator stack not empty and current operator is '*'or '/' check:
+        if top of operator stack is '+' or '-' then push the operator in the operator stack
+        
+        else if top of operator stack is '*' or '/' then pop value stack -> pop operator -> perform operation -> push value and operator*/
+
+
+        if (isOperator(expr[i])) {      
+            while (operator_stack_top >= 0 && ((expr[i] == '+' || expr[i] == '-') || ((expr[i] == '*' || expr[i] == '/') && (operator_stack[operator_stack_top] == '*' || operator_stack[operator_stack_top] == '/')) )) {
+                if (value_stack_top < 1) {
                     printf("Error: Invalid expression.\n");
-                    exit(1);
+                    exit(1);                                        
                 }
-                int b = val[tp_val--];
-                int a = val[tp_val--];
-                val[++tp_val] = Op_eval(a, b, ops[tp_op--]);
+                int b = value_stack[value_stack_top--];
+                int a = value_stack[value_stack_top--];
+                value_stack[++value_stack_top] = evaluateSingleOperation(a, b, operator_stack[operator_stack_top--]);
             }
-            ops[++tp_op] = expr[i];  
+            operator_stack[++operator_stack_top] = expr[i];  
         } 
         else if (isdigit(expr[i])) {
             int num = 0;
+
+            // To consider a multidigit nnumber example - 586
             while (isdigit(expr[i])) {
                 num = num * 10 + (expr[i] - '0');
                 i++;
             }
+
             i--;
-            val[++tp_val] = num;
+            value_stack[++value_stack_top] = num;
         } 
         else {
             printf("Error: Invalid expression.\n");
@@ -66,25 +77,25 @@ int Exp_sol(char* expr) {
         }
     }
 
-    while (tp_op >= 0) {
-        if (tp_val < 1) {
+    while (operator_stack_top >= 0) {
+        if (value_stack_top < 1) {
             printf("Error: Invalid expression.\n");
             exit(1);
         }
-        int b = val[tp_val--];
-        int a = val[tp_val--];
-        val[++tp_val] = Op_eval(a, b, ops[tp_op--]);
+        int b = value_stack[value_stack_top--];
+        int a = value_stack[value_stack_top--];
+        value_stack[++value_stack_top] = evaluateSingleOperation(a, b, operator_stack[operator_stack_top--]);
     }
 
-    return val[tp_val];
+    return value_stack[value_stack_top];
 }
 
 int main() {
     char expression[MAX];
     printf("Enter the expression: ");
-    fgets(expression, MAX, stdin);
+    scanf("%s", expression);
 
-    int result = Exp_sol(expression);
+    int result = solveCompleteExpression(expression);
 
     printf("The result is : %d\n", result);
 
